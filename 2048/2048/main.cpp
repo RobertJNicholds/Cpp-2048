6e41e0d7c998ccd1d9b7dd42dd03cb1c20fc523d
RobertJNicholds <robert.james.nicholds@googlemail.com>
2016-10-10 21:15:35 +0100

    Finished pairing values

--- 2048/2048/main.cpp
+++ 2048/2048/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 void create_grid(int grid[][5]);
 void display_grid(int grid[][5]);
-void move();
+void pair_values(int grid[][5], char direction);
+
+bool running = true;
 
 int main()
 {	
+	
+	srand(static_cast<unsigned int>(time(NULL)));
 	int grid[5][5];
 	create_grid(grid);
-	display_grid(grid);
+
+	while (running)
+	{
+		display_grid(grid);
+		char input;
+		cin >> input;		
+		pair_values(grid, input);
+	}
 
 	int exit;
 	cin >> exit;
@@ -34,11 +46,15 @@ void create_grid(int grid[][5])
 
 		if(grid[x][y] == 0)
 			grid[x][y] = 2;
+
+		grid[0][0] = 2;
+		grid[0][1] = 2;
 	}
 }
 
 void display_grid(int grid[][5])
 {
+	system("cls");
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
@@ -50,17 +66,69 @@ void display_grid(int grid[][5])
 	}
 }
 
-void move(int grid[][5], int direction)
+void pair_values(int grid[][5], char direction)
 {
 	switch (direction)
 	{
 	//UP
-	case 0:
-	//RIGHT
-	case 1:
-	//DOWN
-	case 2:
+	case 'w':
+		for (int x = 0; x < 5; x++)
+		{
+			for (int y = 0; y < 5; y++)
+			{
+				if (grid[x][y] == grid[x - 1][y])
+				{
+					grid[x - 1][y] += grid[x][y];
+					grid[x][y] = 0;						
+				}	
+			}
+		} 		
+		break;
+
 	//LEFT
-	case 3:
+	case 'a':
+		for (int x = 0; x < 5; x++)
+		{
+			for (int y = 0; y < 5; y++)
+			{
+				if (grid[x][y] == grid[x][y - 1])
+				{
+					grid[x][y - 1] += grid[x][y];
+					grid[x][y] = 0;
+				}
+			}
+		}
+		break;
+	//DOWN
+	case 's':
+		for (int x = 0; x < 5; x++)
+		{
+			for (int y = 0; y < 5; y++)
+			{
+				if (grid[x][y] == grid[x + 1][y])
+				{
+					grid[x + 1][y] += grid[x][y];
+					grid[x][y] = 0;
+				}
+			}
+		}
+		break;
+	//RIGHT
+	case 'd':
+		for (int x = 0; x < 5; x++)
+		{
+			for (int y = 0; y < 5; y++)
+			{
+				if (grid[x][y] == grid[x][y + 1])
+				{
+					grid[x][y + 1] += grid[x][y];
+					grid[x][y] = 0;
+				}
+			}
+		}
+		break;
+
+	case 'e':
+		running = false;		
 	}
 }
